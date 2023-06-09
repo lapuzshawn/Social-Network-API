128d1d2b73ddd4fae5daabcfc17047defba5719d
Shawn Lapuz <lapuzshawn@gmail.com>
2023-06-08 18:07:44 -0700

    updated: controllers and routes

--- Develop/routes/api/thought-routes.js
+++ Develop/routes/api/thought-routes.js
@@ -10,10 +10,15 @@ const {
 } = require('../../controllers/thought-controller');
 
 // /api/thoughts
-router.route('/').get(getThoughts).post(createThought);
+router.route('/')
+  .get(getThoughts)
+  .post(createThought);
 
 // /api/thoughts/:thoughtId
-router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
 router.route('/:thoughtId/reactions').post(addReaction);
